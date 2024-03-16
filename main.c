90740465da6ed6c18ee3821a5095c9074879f31f
Jiahan William Wen <wenjiahan2002@gmail.com>
2024-03-16 10:43:48 -0400

    Combined game logic and bg drawing in to one file

--- main.c
+++ main.c
@@ -2,6 +2,16 @@
 // So that can copy and paste into CPULator and run simulations
 // Command + F "coding starts here" to skip back ground color array
 
+// You can fold this source for cleaner screen
+
+//******* include library here *******//
+#include <stdbool.h>
+#include <stdio.h>
+
+//******* define Macros here *******//
+#define MAX 11
+
+//******* Resources put here *******//
 
 const unsigned short bg[76800]={
 0xDFDF, 0xDFDF, 0xE7DF, 0xE7DF, 0xE7DF, 0xE7DF, 0xE7DF, 0xDFDF, 0xDFDF, 0xE7DF, 0xE7BF, 0xDFBF, 0xDFBF, 0xDFBF, 0xDFDF, 0xDFBF,   // 0x0010 (16) pixels
@@ -4808,6 +4818,8 @@ const unsigned short bg[76800]={
 
 // coding starts here
 
+//***************** VGA Stuff *****************//
+
 void plot_pixel(int x, int y, short int line_color);
 
 void clear_screen();
@@ -4815,6 +4827,28 @@ void clear_screen();
 void draw_background();
 
 int pixel_buffer_start;  // global variable
+
+//***************** Game Logic *****************//
+//******* Named Constants *******//
+const int UNUSED = -1;
+const int EMPTY = 0;
+const int LEFT = 1;
+const int RIGHT = 2;
+
+//******* Global Variables *******//
+int array[MAX] = {0};
+int g_num_stones = 5;
+
+//******* Function Declaration *******//
+void clear_array();
+void initialize_array(int num_stones);
+void print_array();
+void print_game_state();
+int validate_move(int frog_to_move);
+int make_move(int to_move, int move);
+int get_input();
+bool check_win();
+bool check_lose();
 
 int main(void) {
   volatile int *pixel_ctrl_ptr = (int *)0xFF203020;
@@ -4824,8 +4858,178 @@ int main(void) {
   clear_screen();
   draw_background();
 	
+  clear_array();
+  initialize_array(g_num_stones);
+  while (1) {
+    print_game_state();
+    int to_move = get_input();
+    printf("You haved selected %d\n", to_move);
+    if (to_move == -1) {  // mistake restart the game
+      initialize_array(g_num_stones);
+      continue;
+    }
+    int move = validate_move(to_move);
+    // printf("Player move is %d\n", move);
+    if (move == 0) {
+      continue;  // don't make a change
+    } else {
+      make_move(to_move, move);
+    }
+    if (check_win()) {
+      //printf("now! try a more difficult one!\n");
+      if (g_num_stones == MAX) {
+        //printf("You have nailed it all! \n");
+        break;
+      }
+      g_num_stones += 2;
+      initialize_array(g_num_stones);
+      continue;
+    } else if (check_lose()) {
+      initialize_array(g_num_stones);
+    }
+  }
+
+  return 0;
 }
 
+//******* Function Definition *******//
+
+//***************** Game Logic FUnction Definition ****************//
+
+void clear_array() {
+  for (int i = 0; i < MAX; i++) {
+    array[i] = UNUSED;
+  }
+}
+
+void initialize_array(int num_stones) {
+  int num_each_side = (int)(num_stones - 1) / 2;
+  for (int i = 0; i < num_each_side; i++) {
+    array[i] = LEFT;
+  }
+  array[num_each_side] = EMPTY;
+  for (int i = num_each_side + 1; i < num_stones; i++) {
+    array[i] = RIGHT;
+  }
+}
+
+void print_array() {
+  for (int i = 0; i < MAX; i++) {
+    printf("%d", array[i]);
+  }
+  printf("\n");
+}
+
+void print_game_state() {
+  for (int i = 1; i < g_num_stones + 1; i++) {  // notice 1 and +1
+    printf("%d ", i);
+  }
+  printf("\n");
+
+  for (int i = 0; i < MAX; i++) {
+    if (array[i] == UNUSED) {
+      break;
+    }
+    if (array[i] == 1) {
+      printf("L ");  // Left frog
+    } else if (array[i] == 2) {
+      printf("R ");  // Right frog
+    } else {
+      printf("_ ");  // Empty stone
+    }
+  }
+  printf("\n");
+}
+
+int validate_move(int to_move) {
+  to_move -= 1;  // to align user input with array
+  //** assume user input will be in range, otherwise need some handler here **//
+  if (array[to_move] == EMPTY) {
+    // printf("This is an empty stone, can't move!\n");
+    return 0;  // empty stone
+  }
+  if (array[to_move] == LEFT) {
+    if (to_move == g_num_stones - 1) {
+      // printf("Can't move, Left has jumped to the right most\n");
+      return 0;
+    } else if (to_move == g_num_stones - 2) {
+      if (array[g_num_stones - 1] != EMPTY) {
+        // printf("Can't move, Left has jumped to the second right most\n");
+        return 0;
+      } else {
+        return 1;
+      }
+    }
+
+    if (array[to_move + 1] == EMPTY) {
+      return 1;  // can move one right
+    } else if (array[to_move + 2] == EMPTY) {
+      return 2;  // can jump two right
+    } else {
+      // printf("can't move\n");
+      return 0;  // can't move
+    }
+  } else if (array[to_move] == RIGHT) {
+    if (to_move == 0) {
+      // printf("Can't move, Right has jumped to the left most\n");
+      return 0;
+    } else if (to_move == 1) {
+      if (array[0] != EMPTY) {
+        // printf("Can't move, Right has jumped to the second left most\n");
+        return 0;
+      } else {
+        return -1;
+      }
+    }
+
+    if (array[to_move - 1] == EMPTY) {
+      return -1;  // can move one right
+    } else if (array[to_move - 2] == EMPTY) {
+      return -2;  // can jump two right
+    } else {
+      // printf("can't move\n");
+      return 0;  // can't move
+    }
+  }
+  return 0;
+}
+
+int make_move(int to_move, int move) {
+  to_move -= 1;
+  array[to_move + move] = array[to_move];
+  array[to_move] = EMPTY;
+  // printf("Made a move\n");
+  return 1;
+}
+
+int get_input() {
+  int number = -1;
+  scanf("%d", &number);
+  return number;
+}
+
+bool check_win() {
+  int num_each_side = (int)(g_num_stones - 1) / 2;
+  for (int i = 0; i < num_each_side; i++) {
+    if (array[i] != RIGHT) return false;
+  }
+  if (array[num_each_side] != EMPTY) return false;
+  for (int i = num_each_side + 1; i < g_num_stones; i++) {
+    if (array[i] != LEFT) return false;
+  }
+  printf("Hey! You solved this puzzle!\n");
+  return true;
+}
+
+bool check_lose() {
+  for (int i = 0; i < g_num_stones; i++) {
+    if (validate_move(i)) return false;
+  }
+  printf("you lost! Try again!\n");
+  return true;
+}
+
+//***************** VGA Function Definition *****************//
 void plot_pixel(int x, int y, short int line_color) {
   volatile short int *one_pixel_address;
 
